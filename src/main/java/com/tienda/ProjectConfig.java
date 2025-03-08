88806ee9abc0ada288ebf5f01d32dd370553e34b
Llerym <69778902+Llerym@users.noreply.github.com>
2025-03-07 20:56:06 -0600

    sem07

---
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author llech
+ */
+public class ProjectConfig implements WebMvcConfigurer{
+    
+}
